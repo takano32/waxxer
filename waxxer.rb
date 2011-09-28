f00a9b428c655fd9f284daa1cf3317d8514088d5
takano32 <mitsuhiro.takano@gree.co.jp>
2011-09-28 16:13:06 +0900

    add file waxxer.rb

---
+++ waxxer.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# vim: noet sts=4:ts=4:sw=4
+# author: takano32 <tak at no32.tk>
+#
+
+class Waxxer
+	def initialize
+	end
+end
+
+class Waxxer::Wax
+	# status(id = rand)
+end
+
+class Waxxer::TwilogWax < Waxxer::Wax
+end
+
+class Waxxer::FavotterWax < Waxxer::Wax
+end
+
+if __FILE__ == $0 then
+end
+
